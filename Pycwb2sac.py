775177443ac2da337a1060cb0b0825a48a264f7a
lindsay <kanglianan@gmail.com>
2019-06-24 10:49:43 -0400

    Add files via upload

---
+++ Pycwb2sac.py
@@ -0,0 +1,97 @@
+#!/Users/lindsaychuang/miniconda3/envs/obspy/bin/python
+
+import sys
+from Pycwb2sac_lib import *
+from obspy.core import Stream
+
+# ---- 01. Ask user for inputs ---------------------------
+if len(sys.argv) == 4:
+    D_file_name = sys.argv[1]
+    Station_file_name = sys.argv[2]
+    Output_format = sys.argv[3]
+    add_arrivals = "n"
+elif len(sys.argv) == 5:
+    D_file_name = sys.argv[1]
+    Station_file_name = sys.argv[2]
+    Output_format = sys.argv[3]
+    P_file_name = sys.argv[4]
+    add_arrivals = "y"
+else:
+    print("Please enter three or four variables. i.e. Pycwb2sac <Dfile> "
+          "<NSTA.DAT> <output_format SAC:0, Obspy:1> <Pfile(optional)>")
+    sys.exit()
+
+print(f'====== converting {D_file_name} =======')
+
+# ---- 02. define file format ------------------------------
+D_file_version = {"Vax": 1311, "PC": 1311}
+segment_length = 1311
+header_length = 15
+sta_num_limit = 200
+
+# ---- 03. Read in Station files ---------------------------
+station_info_file = open(Station_file_name)
+whole_list_reformat = load_station_file(station_info_file)
+print(f'- read Station file: {Station_file_name}')
+
+# ---- 04. Read in .D file ----------------------------------
+with open(D_file_name) as f:
+    D_file_content = np.fromfile(f, dtype=np.int16)
+print(f'- read Dfile: {D_file_name}')
+
+# ---- 04-2 Read in .P file (optional) -----------------------
+if add_arrivals == "y":
+    print(f'- read Pfile: {P_file_name}')
+    P_file_content = open(P_file_name)
+    reformat_p_file = load_p_file(P_file_content)
+else:
+    pass
+
+# ---- 05. Analyze and re-organize D file contents ----------
+format_flag = check_file_type(D_file_content[0])
+print(f'- Dfile {D_file_name} format: {format_flag}')
+# ---- Append file to length of length * d
+print(f'- processing Dfile: {D_file_name}')
+D_file_content_append = append_file(D_file_content, segment_length)
+# ---- Slice long data array and make it to a big matrix
+Data_matrix = break_file_into_pieces(D_file_content_append, segment_length)
+# ---- Trim data matrix base on station number
+Data_matrix = trim_data_matrix(Data_matrix, sta_num_limit)
+# ---- map blocks
+header_one_block = Data_matrix[:, 0:3]
+breaking_points = map_blocks(Data_matrix[:, 0:3])
+
+# ---- 06. Process D-file blocks and write to output
+obspy_stream = Stream()
+for i in range(0, len(breaking_points)):
+    # ----- extract data block
+    Data_block = Data_matrix[breaking_points[i][0]:breaking_points[i][1], :]
+    # ----- re-format header
+    Data_header = reformat_data_header(Data_block[0, 0:15])
+    # ----- extract trace header by comparing two tables
+    trace_header = map_blocks_header(whole_list_reformat, Data_header)
+    # ----- output sac format
+    if int(Output_format) is 0:
+        sac_trace = make_sac_trace(Data_block, trace_header, segment_length - header_length)
+        # ----- add phase arrivals
+        if add_arrivals == "y":
+            sac_trace = add_arrival_to_sac_trace(sac_trace, reformat_p_file)
+        # ----- write to sac file
+        sac_f_name = f'{trace_header.network}.{trace_header.station}.{trace_header.instrument_type}.' \
+            f'{trace_header.channel}.SAC'
+        print(f'- write to {sac_f_name}')
+        sac_trace.write(sac_f_name)
+    # ------ output obspy stream format
+    elif int(Output_format) is 1:
+        obspy_trace = make_obspy_trace(Data_block, trace_header, segment_length - header_length)
+        # ----- add phase arrivals
+        if add_arrivals == "y":
+            obspy_trace = add_arrival_to_obspy_trace(obspy_trace[0], reformat_p_file)
+        obspy_stream += obspy_trace
+    else:
+        raise AttributeError("Unknown output format")
+# ----- write to obspy stream format
+if int(Output_format) is 1:
+    f_name = f'{obspy_stream[0].stats.starttime}_{obspy_stream[0].stats.endtime}.pk'
+    print(f'- write to obspy stream file: {f_name}')
+    obspy_stream.write(f_name, format="PICKLE")
---
+++ Pycwb2sac_lib.py
@@ -0,0 +1,265 @@
+import numpy as np
+from obspy.core import Trace, Stats, UTCDateTime
+
+
+def check_file_type(fnum):
+    if fnum == 0:
+        return "Vax"
+    else:
+        return "PC"
+
+
+def append_file(d_file_content, segment_length):
+    remainder = int(len(d_file_content) % segment_length)
+    if remainder == 0:
+        d_file_content_append = d_file_content
+    else:
+        d_file_content_append = np.append(d_file_content, np.zeros((1, segment_length-remainder), dtype=int))
+
+    return d_file_content_append
+
+
+def break_file_into_pieces(d_file_content_append, file_length):
+    segment_number = int(len(d_file_content_append)/file_length)
+    data_matrix = d_file_content_append.reshape(segment_number, file_length)
+    return data_matrix
+
+
+def trim_data_matrix(data_matrix, sta_num_limit):
+    station_ids = data_matrix[:, 1]
+    remove_row_index = np.argwhere(station_ids > sta_num_limit)
+    return np.delete(data_matrix, remove_row_index, 0)
+
+
+def map_blocks(header_one_block):
+    breaking_points = []
+    b_position = 0
+    for i in range(header_one_block.shape[0]-1):
+        break_flag = np.array_equal(header_one_block[i, 0:3], header_one_block[i+1, 0:3])
+        if break_flag is False:
+            e_position = i
+            breaking_points.append((b_position, e_position))
+            b_position = i+1
+    return breaking_points
+
+
+def load_station_file(station_info_file):
+    whole_list = np.asarray(station_info_file.read().splitlines())
+    whole_list_reformat = []
+    for i in range(len(whole_list)):
+        Station_name = whole_list[i][0:4].strip()
+        Station_indx = int(whole_list[i][28:31])
+        Station_lats = int(whole_list[i][4:6]) + float(whole_list[i][6:11])/60
+        Station_lons = int(whole_list[i][12:15]) + float(whole_list[i][15:20]) / 60
+        Station_elev = float(np.char.strip(whole_list[i][21:27]))
+        Station_begt = fix_station_op_time(str(whole_list[i][66:72]))
+        Station_endt = fix_station_op_time(str(whole_list[i][73:79]))
+        whole_list_reformat.append({"station": f'{Station_name:s}', "stationid": f'{Station_indx:d}',
+                                     "stla": f'{Station_lats:.4f}', "stlo": f'{Station_lons:.4f}',
+                                     "stel": f'{Station_elev:.2f}', "stbt": Station_begt, "stet": Station_endt})
+    return whole_list_reformat
+
+
+def fix_station_op_time(station_be_time):
+    year = int(station_be_time[0:2])
+    mont = int(station_be_time[2:4])
+    dayt = int(station_be_time[4:6])
+    if year == 0 and mont == 0 and dayt == 0:
+        time = UTCDateTime(1900, 1, 1)
+    elif year == 99 and mont == 24 and dayt == 31:
+        year = year + 2000
+        time = UTCDateTime(year, 12, 31)
+    elif mont > 12:
+        year = year + 2000
+        mont = mont - 12
+        time = UTCDateTime(year, mont, dayt)
+    elif year >= 60:
+        year = year + 1900
+        time = UTCDateTime(year, mont, dayt)
+    else:
+        raise AttributeError("Please check station operation time format")
+    return time
+
+
+def reformat_data_header(data_header_block):
+    if len(data_header_block) != 15:
+        raise AttributeError("data header format error")
+    else:
+        if len(str(data_header_block[4])) == 2:
+            year = data_header_block[4] + 1900
+        else:
+            year = data_header_block[4]
+        stid = int(data_header_block[1])
+        stcp = int(data_header_block[2])
+        stsp = int(data_header_block[3])
+        stin = int(data_header_block[13])
+        mont = int(f'{data_header_block[5]:04d}'[0:2])
+        dayt = int(f'{data_header_block[5]:04d}'[2:4])
+        hour = int(data_header_block[6])
+        mins = int(data_header_block[7])
+        secs = int(data_header_block[8]) + int(data_header_block[9])/stsp
+        time = UTCDateTime(year, mont, dayt, hour, mins, secs)
+        if stcp == 1:
+            stcptr = "Z"
+        elif stcp == 2:
+            stcptr = "N"
+        elif stcp == 3:
+            stcptr = "E"
+        else:
+            raise AttributeError("component format error")
+        if stin == 0:
+            instrument_type = "S13"
+        elif stin == 1:
+            instrument_type = "RTD"
+        elif stin == 2:
+            instrument_type = "BB_V"
+        elif stin == 3:
+            instrument_type = "FBA"
+        else:
+            raise AttributeError("instrument type error")
+
+        data_block_headers = {"stationid": stid, "stationcp": stcp, "sampling_rate": stsp,
+                              "starttime": time, "channel": stcptr, "instrument_type": instrument_type}
+    return data_block_headers
+
+
+def map_blocks_header(whole_list_reformat, data_block_headers):
+    network = "CWBSN"
+    station_id = str(data_block_headers["stationid"])
+    event_time = data_block_headers["starttime"]
+    channel = data_block_headers["channel"]
+    sampling_rate = data_block_headers["sampling_rate"]
+    starttime = data_block_headers["starttime"]
+    instrument_type = data_block_headers["instrument_type"]
+    list_of_single_station = list(filter(lambda staid: staid['stationid'] == station_id, whole_list_reformat))
+    list_gt_evt_time = list(filter(lambda stabt: stabt['stbt'] <= event_time, list_of_single_station))
+    list_lt_evt_time = list(filter(lambda stabt: stabt['stet'] >= event_time, list_gt_evt_time))
+    if len(list_lt_evt_time) != 1:
+        raise AttributeError("station operational date ambiguity")
+    else:
+        station = list_lt_evt_time[0]["station"]
+        stla = list_lt_evt_time[0]["stla"]
+        stlo = list_lt_evt_time[0]["stlo"]
+        stel = list_lt_evt_time[0]["stel"]
+        trace_stats = Stats(header={"station": station, "sampling_rate": sampling_rate,
+                                    "starttime": starttime, "channel": channel, "network": network,
+                                    "stla": stla, "stlo": stlo, "stel": stel, "instrument_type": instrument_type})
+    return trace_stats
+
+
+def make_sac_trace(data_block, sac_header, data_length):
+    from obspy.io.sac import SACTrace
+    flatten_data = np.concatenate(data_block[..., 15::])
+    sac_header.npts = flatten_data.size
+    new_trace = Trace(data=flatten_data, header=sac_header)
+    sac_trace = SACTrace.from_obspy_trace(new_trace)
+    sac_trace.stla = sac_header.stla
+    sac_trace.stlo = sac_header.stlo
+    sac_trace.stel = sac_header.stel
+    if int(new_trace.stats.npts % data_length) is not 0:
+        raise AttributeError("data format error!")
+    else:
+        return sac_trace
+
+
+def make_obspy_trace(data_block, sac_header, data_length):
+    from obspy.core import Stream
+    flatten_data = np.concatenate(data_block[..., 15::])
+    sac_header.npts = flatten_data.size
+    new_trace = Trace(data=flatten_data, header=sac_header)
+    if int(new_trace.stats.npts % data_length) is not 0:
+        raise AttributeError("data format error!")
+    else:
+        return Stream(traces=new_trace)
+
+
+def add_arrival_to_sac_trace(sac_trace, reformat_p_file):
+    station = sac_trace.kstnm
+    old_reference_time = sac_trace.reftime
+    arrival = list(filter(lambda sta: sta['station'] == station, reformat_p_file))
+    sac_trace.o = reformat_p_file[0]["o"]
+    if arrival:
+        sac_trace.t1 = arrival[0]["t1"]
+        sac_trace.t2 = arrival[0]["t2"]
+        sac_trace.evla = reformat_p_file[0]["evla"]
+        sac_trace.evlo = reformat_p_file[0]["evlo"]
+        sac_trace.evdp = reformat_p_file[0]["evdp"]
+    sac_trace.iztype = "io"
+    return sac_trace
+
+
+def add_arrival_to_obspy_trace(obspy_trace, reformat_p_file):
+    from obspy.core import Stream
+    station = obspy_trace.stats.station
+    arrival = list(filter(lambda sta: sta['station'] == station, reformat_p_file))
+    obspy_trace.stats.o = reformat_p_file[0]["o"]
+    if arrival:
+        obspy_trace.stats.t1 = arrival[0]["t1"]
+        obspy_trace.stats.t2 = arrival[0]["t2"]
+        obspy_trace.stats.evla = reformat_p_file[0]["evla"]
+        obspy_trace.stats.evlo = reformat_p_file[0]["evlo"]
+        obspy_trace.stats.evdp = reformat_p_file[0]["evdp"]
+    return Stream(traces=obspy_trace)
+
+
+def load_p_file(p_file_content):
+    p_file_list = p_file_content.read().splitlines()
+    reformat_p_file = []
+    evt_dic = reformat_p_file_header(p_file_list[0])
+    reformat_p_file.append(evt_dic)
+    for i in range(1, len(p_file_list)):
+        if int(len(p_file_list[i])) is 75 or int(len(p_file_list[i])) is 83:
+            station = p_file_list[i][1:5].strip()
+            p_arrival_time = evt_dic["o"] - evt_dic["o"].minute*60 - evt_dic["o"].second - \
+                             evt_dic["o"].microsecond/1e6 + float(p_file_list[i][23:29]) + int(p_file_list[i][21:23])*60
+            s_arrival_time = evt_dic["o"] - evt_dic["o"].minute*60 - evt_dic["o"].second - \
+                             evt_dic["o"].microsecond/1e6 + float(p_file_list[i][39:45]) + int(p_file_list[i][21:23])*60
+            if s_arrival_time <= p_arrival_time:
+                s_arrival_time = -12345
+            reformat_p_file.append({"station": station, "t1": p_arrival_time, "t2": s_arrival_time, "o": evt_dic["o"]})
+
+        else:
+            raise AttributeError("P file arrival time format incorrect")
+    return reformat_p_file
+
+
+def reformat_p_file_header(p_file_header):
+    if int(len(p_file_header)) is 72:
+        year = int(p_file_header[1:5])
+        mont = int(p_file_header[5:7])
+        dayt = int(p_file_header[7:9])
+        hour = int(p_file_header[9:11])
+        mint = int(p_file_header[11:13])
+        secs = float(p_file_header[13:19])
+        time = UTCDateTime(year, mont, dayt, hour, mint, secs)
+        evla = float(p_file_header[19:21]) + float(p_file_header[21:26]) / 60
+        evlo = float(p_file_header[26:29]) + float(p_file_header[29:34]) / 60
+        evdp = float(p_file_header[34:40])
+        magn = float(p_file_header[40:44])
+        evgp = int(p_file_header[51:54])
+        evrm = float(p_file_header[54:58])
+        errh = float(p_file_header[58:62])
+        errz = float(p_file_header[62:66])
+        evt_info = {"station": None, "o":time, "evla": evla, "evlo": evlo, "evdp": evdp, "mag": magn,
+                    "evrms": evrm, "everror_h": errh, "evrror_z": errz}
+    elif int(len(p_file_header)) is 70:
+        year = int(p_file_header[1:3]) + 1900
+        mont = int(p_file_header[3:5])
+        dayt = int(p_file_header[5:7])
+        hour = int(p_file_header[7:9])
+        mint = int(p_file_header[9:11])
+        secs = float(p_file_header[11:17])
+        time = UTCDateTime(year, mont, dayt, hour, mint, secs)
+        evla = float(p_file_header[17:19]) + float(p_file_header[19:24]) / 60
+        evlo = float(p_file_header[24:27]) + float(p_file_header[27:32]) / 60
+        evdp = float(p_file_header[32:38])
+        magn = float(p_file_header[38:42])
+        evgp = int(p_file_header[49:52])
+        evrm = float(p_file_header[52:56])
+        errh = float(p_file_header[56:60])
+        errz = float(p_file_header[60:64])
+        evt_info = {"station": None, "o":time, "evla": evla, "evlo": evlo, "evdp": evdp, "mag": magn,
+                    "evrms": evrm, "everror_h": errh, "evrror_z": errz}
+    else:
+        raise AttributeError("p file header format incorrect")
+    return evt_info
